54263be628368ff9693ccc1276154a8941ec025e
yijie99 <34464132+yijie99@users.noreply.github.com>
2018-01-03 17:54:29 +0800

    finish static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
+  root 'pages#home'
+  get '/about', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get pages_home_url
+  
+  def setup
+  	@base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get root_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
-    get pages_about_url
+    get about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
