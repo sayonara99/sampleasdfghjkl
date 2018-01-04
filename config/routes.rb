de242a28152d1e4f6093e2d06d2d209135937c49
yijie99 <34464132+yijie99@users.noreply.github.com>
2018-01-04 13:25:05 +0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/about', to: 'pages#about'
   get '/signup', to: 'users#signup'
+  post '/signup', to: 'users#create'
   resources :users
 
 end
