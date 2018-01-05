4fd756c2c6780de8b44551698e00bf6c58e6bda0
yijie99 <34464132+yijie99@users.noreply.github.com>
2018-01-05 11:27:27 +0800

    implement advanced login with cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  	  log_in user
-  	  redirect_to user
+  	@user = User.find_by(email: params[:session][:email].downcase)
+  	if @user && @user.authenticate(params[:session][:password])
+  	  log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      # if '1', remember user, else, forget user
+  	  redirect_to @user
   	  # same as   user_url(user)
   	elsif params[:session][:email].blank?
   		flash.now[:danger] = "Enter your email and password"
@@ -22,7 +24,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 # by writing SQL statesments(if the database is SQL), and Active Record lets you interact with that data as though
 # it was a normal Ruby object.
 
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
   # the before_save callback is automatially called BEFORE the object is saved, which includes both
   # object creation and update
@@ -41,5 +43,26 @@ class User < ApplicationRecord
     # the computational cost to calculate the hash
   end
 
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
 end
